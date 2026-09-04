70b64e3af9c9e4a359bbf84242b3a96a36db2ba7
RenOmstead <renomstead@gmail.com>
2026-09-03 20:33:12 -0400

    Create floor-layout.js

---
+++ js/floor-layout.js
@@ -0,0 +1,27 @@
+window.APTA_FLOOR_LAYOUT = [
+    // We will put every mapped booth here.
+
+    {
+        booth_number: "3186",
+        x: 18,
+        y: 4,
+        width: 1,
+        depth: 1
+    },
+
+    {
+        booth_number: "4279",
+        x: 34,
+        y: 24,
+        width: 1,
+        depth: 1
+    },
+
+    {
+        booth_number: "4614",
+        x: 37,
+        y: 31,
+        width: 3,
+        depth: 1
+    }
+];
